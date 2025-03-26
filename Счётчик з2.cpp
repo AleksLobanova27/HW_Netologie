951495757d130d077c14b003f7584d891d71a882
Aleksandra <elaginas27@gmail.com>
2025-03-26 12:18:55 +0300

    commit 4

--- Счётчик з2.cpp
+++ Счётчик з2.cpp
@@ -5,12 +5,13 @@ class Counter {
 private:
     int _num;
 public:
-    Counter() {_num = 1;}
-    void set_num(int num) { _num = num; }
+    Counter() { _num = 1; }
+    Counter(int num) { _num = num; }
     void set_plus() { _num++; }
     void set_minus() { _num--; }
-    int get_num() { 
-        return _num; }
+    int get_num() {
+        return _num;
+    }
 
 
 };
@@ -25,17 +26,17 @@ int main()
         int num;
         cout << "Введите начальное значение счётчика: ";
         cin >> num;
-        number.set_num(num);
+        number = Counter(num);
     }
     string znak;
     while (znak != "x") {
         cout << "Введите команду('+', '-', '=' или 'x') : ";
         cin >> znak;
-        if (znak == "+") { number.set_plus();}
+        if (znak == "+") { number.set_plus(); }
         if (znak == "-") { number.set_minus(); }
         if (znak == "=") { cout << number.get_num() << endl; }
     }
     cout << "До свидания!";
-       
+
 
 }
