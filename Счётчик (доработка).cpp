2b24fdd190f2783ed0731656e286b7f2df650560
Aleksandra <elaginas27@gmail.com>
2025-03-26 12:23:09 +0300

    4commit

---
+++ Счётчик (доработка).cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+class Counter {
+private:
+    int _num;
+public:
+    Counter() { _num = 1; }
+    Counter(int num) { _num = num; }
+    void set_plus() { _num++; }
+    void set_minus() { _num--; }
+    int get_num() {
+        return _num;
+    }
+
+
+};
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "Вы хотите указать начальное значение счётчика? Введите yes или not: ";
+    string answer;
+    cin >> answer;
+    Counter number;
+    if (answer == "yes") {
+        int num;
+        cout << "Введите начальное значение счётчика: ";
+        cin >> num;
+        number = Counter(num);
+    }
+    string znak;
+    while (znak != "x") {
+        cout << "Введите команду('+', '-', '=' или 'x') : ";
+        cin >> znak;
+        if (znak == "+") { number.set_plus(); }
+        if (znak == "-") { number.set_minus(); }
+        if (znak == "=") { cout << number.get_num() << endl; }
+    }
+    cout << "До свидания!";
+
+
+}
